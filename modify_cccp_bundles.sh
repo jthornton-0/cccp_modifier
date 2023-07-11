04427569d08779a38d31d2015111e6853ac746d5
jthornton-0 <pi6688@protonmail.com>
2023-07-11 02:25:22 +0100

    Fixed typo and formatting

--- modify_cccp_bundles.sh
+++ modify_cccp_bundles.sh
@@ -9,9 +9,7 @@
 # Version: 1.0.1
 # License: MIT
 
-
 set -u -e -o pipefail
-
 
 # optional arguments
 _VERBOSE=0         # verbosity level, 0 for output to terminal, 1 for silent
@@ -24,7 +22,6 @@ residues='ala gly' # residue names (currently only supports 'ala' and 'gly'
 # required arguments
 res_per_chain=""        # residues per chain, positive non-zero integer
 path_to_cccp_bundles="" # path to the dir containing the CCCP generated bundles
-
 
 OPTIND=1
 
@@ -50,47 +47,46 @@ if [[ ${#} -eq 0 ]]; then
   exit 1
 fi
 
-while getopts ':hs:z:c:e:a:p:r:v:' option;
-do
+while getopts ':hs:z:c:e:a:p:r:v:' option; do
   case "$option" in
-    h)
-      echo "$usage"
-      exit 1
-      ;;
-    s)
-      seg_id="$OPTARG"
-      ;;
-    z)
-      chains="$OPTARG"
-      ;;
-    c)
-      chain_id="$OPTARG"
-      ;;
-    e)
-      cccp_sep="$OPTARG"
-      ;;
-    a)
-      residues="$OPTARG"
-      ;;
-    p)
-      path_to_cccp_bundles="$OPTARG"
-      ;;
-    r)
-      res_per_chain=$OPTARG
-      ;;
-    v)
-      _VERBOSE=$OPTARG
-      ;;
-    :)
-      printf "missing argument for -%s\n" "$OPTARG" >&2
-      echo "$usage" >&2
-      exit 1
-      ;;
-    \?)
-      printf "illegal option: -%s\n" "$OPTARG" >&2
-      echo "$usage" >&2
-      exit 1
-      ;;
+  h)
+    echo "$usage"
+    exit 1
+    ;;
+  s)
+    seg_id="$OPTARG"
+    ;;
+  z)
+    chains="$OPTARG"
+    ;;
+  c)
+    chain_id="$OPTARG"
+    ;;
+  e)
+    cccp_sep="$OPTARG"
+    ;;
+  a)
+    residues="$OPTARG"
+    ;;
+  p)
+    path_to_cccp_bundles="$OPTARG"
+    ;;
+  r)
+    res_per_chain=$OPTARG
+    ;;
+  v)
+    _VERBOSE=$OPTARG
+    ;;
+  :)
+    printf "missing argument for -%s\n" "$OPTARG" >&2
+    echo "$usage" >&2
+    exit 1
+    ;;
+  \?)
+    printf "illegal option: -%s\n" "$OPTARG" >&2
+    echo "$usage" >&2
+    exit 1
+    ;;
   esac
 done
 
@@ -121,68 +117,65 @@ if [ "$_VERBOSE" != 0 ] && [ "$_VERBOSE" != 1 ]; then
 fi
 
 readonly seg_id chains chain_id cccp_sep residues path_to_cccp_bundles \
-         res_per_chain _VERBOSE
-
+  res_per_chain _VERBOSE
 
 tmpfile="$(mktemp)"
 tmpphnx=phenix.out
 
-
-log () {
+log() {
   if [[ $_VERBOSE -eq 0 ]]; then
     echo "$@"
   fi
 }
 
-mkdir_and_cd () {
-  mkdir -p "$1"; cd "$1"
+mkdir_and_cd() {
+  mkdir -p "$1"
+  cd "$1"
 }
 
-res_one_letter_cap () {
+res_one_letter_cap() {
   local abbrev && abbrev=$(printf %.1s "$1")
   Abbrev="${abbrev^}"
   echo "$Abbrev"
 }
 
 #  write Rosetta residue file for all Ala or Gly
-write_resfiles () {
+write_resfiles() {
   local res_abbrev && res_abbrev=$(res_one_letter_cap "$1")
   local file_name && file_name="all_${1}_resfile.res"
   rm -f "$file_name" # appends if exists)
-  printf $"PIKAA %s\nstart" "$res_abbrev" >> "$file_name"
+  printf $"PIKAA %s\nstart" "$res_abbrev" >>"$file_name"
   log "@>> finished writing $1 resfile"
 }
 
 #  execute Rosetta fix backbone to add side chains and hydrogen's
-exe_Rosetta_fixbb () {
+exe_Rosetta_fixbb() {
   local fullpdbpath && fullpdbpath="$path_to_cccp_bundles"/"$cccp_sep${1}"/
   local numfiles && numfiles=$(find "$fullpdbpath" | wc -l)
   local i && i=0
   SECONDS=0
-  for pdb in "$fullpdbpath"*.pdb;
-  do
+  for pdb in "$fullpdbpath"*.pdb; do
     ((i++))
     filename="${pdb##*/}"
-    log "@>> starting Rosetta fixbb for $filename ($i/$(( numfiles - 1 )))"
+    log "@>> starting Rosetta fixbb for $filename ($i/$((numfiles - 1)))"
     fixbb.default.linuxgccrelease -s "$pdb" -resfile \
-    ../all_"${1}"_resfile.res -nstruct 1 -ex1 -ex2 -database \
-    "$ROSETTA3_DB" > /dev/null
+      ../all_"${1}"_resfile.res -nstruct 1 -ex1 -ex2 -database \
+      "$ROSETTA3_DB" >/dev/null
     log "@>> finished Rosetta fixbb for $filename"
   done
   DURATION=$SECONDS
   log "@>> Rosetta fixbb for $1 finished in ~$DURATION seconds"
 }
 
-run_Rosetta_fixbb () {
-  mkdir_and_cd ./poly_"${1}" && exe_Rosetta_fixbb "$1" && \
+run_Rosetta_fixbb() {
+  mkdir_and_cd ./poly_"${1}" && exe_Rosetta_fixbb "$1" &&
     rm score.sc && cd ../ || exit
 }
 
-rename_pdbs () {
+rename_pdbs() {
   log "@>> renaming ${1} pdbs"
   f=0
-  for pdb in ./poly_"${1}"/*.pdb;
-  do
+  for pdb in ./poly_"${1}"/*.pdb; do
     ((++f)) # increment first or it doesn't work
     ext="${pdb##*.}"
     printf -v j "%04d" "$f"
@@ -193,35 +186,33 @@ rename_pdbs () {
   log "@>> finished renaming ${1} pdbs"
 }
 
-mod_segment_ter_chain_id () {
+mod_segment_ter_chain_id() {
   log "@>> removing TER lines"
   sed -i "/TER/d" $tmpphnx
   log "@>> changing chains to $chain_id"
   awk '{print substr($0,1,21) v substr($0,23)}' v="$chain_id" $tmpphnx \
-    > "$tmpfile" && mv "$tmpfile" $tmpphnx
+    >"$tmpfile" && mv "$tmpfile" $tmpphnx
   log "@>> changing segment IDs to $seg_id"
   awk '{print substr($0,1,75) v substr($0,76)}' v="$seg_id" $tmpphnx \
-    > "$tmpfile" && mv "$tmpfile" $tmpphnx
+    >"$tmpfile" && mv "$tmpfile" $tmpphnx
 }
 
-chain_resseq_mod () {
+chain_resseq_mod() {
   phenix.pdbtools "$1" clear_seg_id=true \
-    output.filename=$tmpphnx > /dev/null
+    output.filename=$tmpphnx >/dev/null
   read -ra chain_ar <<<"$chains" && len_chains=${#chain_ar[@]} && i=1 || exit
-  while [ $i -le $((len_chains - 1)) ];
-  do
+  while [ "$i" -le $((len_chains - 1)) ]; do
     log "@>> modifying sequence for chain ${chain_ar[$i]}"
     inc_resseq=$((i * res_per_chain))
     phenix.pdbtools $tmpphnx modify.selection="chain ${chain_ar[$i]}" \
-      increment_resseq=$inc_resseq output.filename=$tmpphnx > /dev/null
+      increment_resseq=$inc_resseq output.filename=$tmpphnx >/dev/null
     ((i++))
   done
 }
 
-run_phenix () {
+run_phenix() {
   mkdir_and_cd ../phenix/
-  for file in ../Rosetta/poly_"${res}"/*.pdb;
-  do
+  for file in ../Rosetta/poly_"${res}"/*.pdb; do
     mkdir -p ../templates/poly_"${res}"
     filename="${file##*/}"
     cp "$file" "$filename"
@@ -235,7 +226,7 @@ run_phenix () {
   done
 }
 
-main () {
+main() {
   local date && date=$(date '+%Y-%m-%d %H:%M:%S')
   local kernal_v && kernal_v=$(uname -a)
   log ""
@@ -246,8 +237,7 @@ main () {
   log "@>> Starting CCCP bundle modifications"
 
   mkdir_and_cd ./Rosetta
-  for res in $residues;
-  do
+  for res in $residues; do
     log "@>> writing resfile for $res"
     write_resfiles "$res"
     log "@>> starting Rosetta fixed backbone for $res"
